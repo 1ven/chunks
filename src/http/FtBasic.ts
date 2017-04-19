07abb2b0b4f51286d02be34515cb61321e7725d4
Ivan Mironchik <me.roaiven@gmail.com>
2017-04-19 16:41:06 +0300

    Implement FtBasic

---
+++ src/http/FtBasic.ts
@@ -0,0 +1,28 @@
+import * as net from 'net';
+import { Front } from './index';
+import { Chunk } from '../chunk';
+import { BkBasic } from './BkBasic';
+
+class FtBasic implements Front {
+  private chunk: Chunk;
+  private port: number;
+
+  constructor(chunk: Chunk, port: number) {
+    this.chunk = chunk;
+    this.port = port;
+  }
+
+  public start() {
+    const server = net.createServer();
+
+    server.on('connection', (socket: net.Socket) => {
+      new BkBasic(this.chunk).accept(socket);
+    });
+
+    server.listen(this.port);
+  }
+}
+
+export {
+  FtBasic,
+}
