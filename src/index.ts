c362570c057993f3eb3d8a6a0c60761a8be279fe
Ivan Mironchik <me.roaiven@gmail.com>
2017-04-20 15:58:20 +0300

    Add exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 // TODO: Make all names shorter, e.x request - req etc
 export { BkBasic, FtBasic } from './http';
+export {
+  RsEmpty,
+  RsJson,
+  RsSimple,
+  RsWrap,
+  RsWithBody,
+  RsWithStatus,
+  RsWithType,
+} from './response';
 
 export { RqLive } from './request/RqLive';
